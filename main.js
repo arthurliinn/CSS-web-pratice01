d538621deb3b093b7de320ef752da1c37d51f48e
arthur lin <arthurliinn@gmail.com>
2019-01-08 15:14:34 +0800

    slider JS修正

--- main.js
+++ main.js
@@ -16,21 +16,26 @@ function sliderSlide() {
 
 setTimeout(sliderSlide(), 5000);
 */
+//window.addEventListener("load", sliderSlide, false);
+var sliderXCurrent = document.getElementById("steppedCarousel_slides");
+//sliderXCurrent.style.transform = "translate3d(0,0,0)";
+console.log("t");
+setInterval("sliderSlide()", 5000);
 
-var slider = document.getElementById("steppedCarousel_slides");
-var sliderImg = 0;
-
-setInterval("sliderSlide()", 1000);
-/*function sliderSlide() {
-  if (sliderImg = 0) {
-    slider.style.transform = "translate3d(-100%,0,0)";
-    setTimeout()
-    sliderImg ++;
-  } else if (sliderImg = 1) {
-    slider.style.transform = "translate3d(-200%,0,0)";
-    sliderImg ++;
-  } else if ((sliderImg = sliderImg[2])) {
-    slider.style.transfornm = "translate3d(0,0,0)";
-    sliderImg = sliderImg[0];
+function sliderSlide() {
+  console.log("test");
+  var sliderXCurrent = document.getElementById("steppedCarousel_slides");
+  //sliderXCurrent.style.transform = "translate3d(0,0,0)";
+  if (sliderXCurrent.style.transform = "translate3d(0%,0,0)") {
+    sliderXCurrent.style.transform = "translate3d(-100%,0,0)";
+    console.log("test01");
+  } else if (sliderXCurrent.style.transform = "translate3d(-100%,0,0)") {
+    sliderXCurrent.style.transform = "translate3d(-200%,0,0)";
+    console.log("test02");
+  } else if (sliderXCurrent.style.transform = "translate3d(-200%,0,0)") {
+    sliderXCurrent.style.transform = "translate3d(0,0,0)";
+    console.log("test03");
   };
-};*/
+};
+
+
