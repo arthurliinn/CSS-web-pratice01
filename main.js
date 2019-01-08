8d404b6f34aee1855b08ab8bdd52f5f25fc9b73b
arthur lin <arthurliinn@gmail.com>
2019-01-08 23:56:59 +0800

    slider成功

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Slider
+/*//Slider
 /*var slider = document.getElementsByClassName("steppedCarousel_slides");
 var sliderImg = [0, 1, 2];
 function sliderSlide() {
@@ -12,10 +12,10 @@ function sliderSlide() {
     slider.style.transfornm = "translate3d(0,0,0)";
     sliderImg = sliderImg[0];
   }
-}
+}*/
 
-setTimeout(sliderSlide(), 5000);
-*/
+//setTimeout(sliderSlide(), 5000);
+
 //window.addEventListener("load", sliderSlide, false);
 var sliderXCurrent = document.getElementById("steppedCarousel_slides");
 //sliderXCurrent.style.transform = "translate3d(0,0,0)";
@@ -25,17 +25,21 @@ setInterval("sliderSlide()", 5000);
 function sliderSlide() {
   console.log("test");
   var sliderXCurrent = document.getElementById("steppedCarousel_slides");
-  //sliderXCurrent.style.transform = "translate3d(0,0,0)";
-  if (sliderXCurrent.style.transform = "translate3d(0%,0,0)") {
-    sliderXCurrent.style.transform = "translate3d(-100%,0,0)";
+  //var x = sliderXCurrent.style.transform.value;
+  var per01 = "translateX(0%)";
+  var per02 = "translateX(-100%)";
+  var per03 = "translateX(-200%)";
+  if (sliderXCurrent.style.transform == per01) {
+    sliderXCurrent.style.transform = per02;
     console.log("test01");
-  } else if (sliderXCurrent.style.transform = "translate3d(-100%,0,0)") {
-    sliderXCurrent.style.transform = "translate3d(-200%,0,0)";
+  } else if (sliderXCurrent.style.transform == per02) {
+    sliderXCurrent.style.transform = per03;
     console.log("test02");
-  } else if (sliderXCurrent.style.transform = "translate3d(-200%,0,0)") {
-    sliderXCurrent.style.transform = "translate3d(0,0,0)";
+  } else if (sliderXCurrent.style.transform == per03) {
+    sliderXCurrent.style.transform = per01;
     console.log("test03");
   };
+  //console.log(x);
 };
 
 
