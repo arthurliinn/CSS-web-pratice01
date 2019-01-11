41b95fa5b565664758135994478732b58a0af705
arthur lin <arthurliinn@gmail.com>
2019-01-12 01:11:04 +0800

    topnav bottomLine JS add

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ function sliderSlide() {
 //setTimeout(sliderSlide(), 5000);
 
 //window.addEventListener("load", sliderSlide, false);
-var sliderXCurrent = document.getElementById("steppedCarousel_slides");
+
 //sliderXCurrent.style.transform = "translate3d(0,0,0)";
 console.log("t");
-setInterval("sliderSlide()", 5000);
+//setInterval("sliderSlide()", 5000);
 
-function sliderSlide() {
+/*function sliderSlide() {
   console.log("test");
   var sliderXCurrent = document.getElementById("steppedCarousel_slides");
   //var x = sliderXCurrent.style.transform.value;
@@ -40,6 +40,42 @@ function sliderSlide() {
     console.log("test03");
   };
   //console.log(x);
+};*/
+
+var percent = [0, -100, -200];
+var currentOffset = 0;
+
+setInterval("sliderSlide()", 5000);
+
+function sliderSlide() {
+  currentOffset = (currentOffset + 1) % + percent.length;
+  var sliderXCurrent = document.getElementById("steppedCarousel_slides");
+  sliderXCurrent.style.transform = "translateX(" + percent[currentOffset] + "%)";
+
 };
 
+
+
+//Topnav bottomLine
+
+var topNav = document.getElementsByName("topnavBtn");
+var i = 0;
+
+for (i = 0; i < topNav.length; i++) {
+  topNav[i].addEventListener("mouseover", moveLine, false);
+
+};
+
+function moveLine() {
+  console.log("hello");
+  var topNav = document.getElementsByName("topnavBtn");
+  var focusLine = document.getElementById("siteNav_focusLine");
+  var i = 0;
+  for (i = 0; i < topNav.length; i++) {
+    var CurrentBtnStyle = topNav[i].style.transform;
+    focusLine.style.transform = CurrentBtnStyle;
+
+  };
+  console.log(CurrentBtnStyle);
+};
 
